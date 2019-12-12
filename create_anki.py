87c2ea99fe339806f2dd1945408787658b5407a5
rafasaurus <rafa.grigorian@gmail.com>
2019-12-12 12:54:24 +0400

    Update working with test.txt

--- create_anki.py
+++ create_anki.py
@@ -23,13 +23,25 @@ my_model = genanki.Model(
 
 my_deck = genanki.Deck(
     2059400110,
-    'IT-Sicherheit')
-with open(Path('IT-Sicherheit-Fragen-TINF15AIBC.txt'), encoding="utf8", errors='ignore') as f:
+    'test')
+with open(Path('test.txt'), encoding="utf8", errors='ignore') as f:
     # contents = Path('IT-Sicherheit-Fragen-TINF15AIBC.txt').read_text()
     contents = f.read()
     n = 0
-    for frage in re.split(r"\n\d+\.", contents):
-        front, back = frage.split("\n", 1)
+    for frage in re.split(r"\d\.\d\/", contents):
+        frage = "\n\n\n\n0.0/" + frage
+        back = re.findall(r"\n\ա\)\S*\s*\n\բ\)\S*\s*\n\գ\)\S*\s*\n\դ\)\S*\s*", frage)
+        front = re.findall(r"\d\.\d\/(?P<name>[Ա-ֆա-ֆ\s\-\d\`\,\/\n .]+)\n\n", frage)
+        if len(back) is not 0:
+            print("back>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", back[0])
+        else:
+            continue
+        if len(front) is not 0:
+            print("front>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", front[0])
+        else:
+            continue
+        front = front[0]
+        back = "\n\n\n" + back[0]
         front = front.strip()
         n += 1
         i = 0
@@ -44,11 +56,11 @@ with open(Path('IT-Sicherheit-Fragen-TINF15AIBC.txt'), encoding="utf8", errors='
             model=my_model,
             fields=[front, back.replace("\n","<br>")])
         my_deck.add_note(my_note)
-        print(back.replace("\n", "<br>"))
+        # print(back.replace("\n", "<br>"))
 
         #with tk(timeout=1.5):
          #   codebox("Contents of file " + filename, "Show File Contents", text)
         #pymsgbox.native.alert(back, front)
         #if(n>15):
          #   print("Frage: ", front, "\n\nAntwort: ", back)
-    genanki.Package(my_deck).write_to_file('it-sicherheit.apkg')
+    genanki.Package(my_deck).write_to_file('test.apkg')
